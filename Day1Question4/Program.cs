3a75192b2cc0c5e3af4bf911c0031f68e5619f84
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-05-10 07:05:54 -0700

    FirstCommit

--- Day1Question4/Program.cs
+++ Day1Question4/Program.cs
@@ -8,34 +8,37 @@ namespace Day1Question4
         static void Main(string[] args)
         {
             List<int> i = new List<int> { 1, 4, 4, 4, 0, 4, 3, 3, 1 };
-            List<int> o = new List<int> { 1, 12, 0, 4, 6, 1 };
             SumConsecutives(i);
-            SumConsecutives(o);
+        
         }
 
         public static List<int> SumConsecutives(List<int> s)
         {
             var arrAfterSum = new List<int>(); 
-            int[] arr = s.ToArray();
+            int[] ListToArray = s.ToArray();
 
-            int sum = 0;
-            int k = 0;
-            for (int i = 0; i < arr.Length-1; i++)
+            for (int i = 0; i < ListToArray.Length; i++)
             {
-                if (k < arr.Length -1)
-                {
-                    sum = arr[k];
-                    if (arr[k] == arr[k + 1])
-                    {
-                        sum += arr[k + 1];
-                        k = k + 1;
-                    }
-                    
-                }
+                int count = 0;
+                int sum = ListToArray[i];
                 
+                for (int j = i + 1; j < ListToArray.Length; j++)
+                {
+                    if (ListToArray[j] == ListToArray[i])
+                    {
+                        sum += ListToArray[j];
+                        count = count + 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
                 arrAfterSum.Add(sum);
+                i =i + count;
             }
-            arrAfterSum.ForEach(i => Console.WriteLine(i.ToString()));
+       
+            //arrAfterSum.ForEach(i => Console.WriteLine(i.ToString()));
             return arrAfterSum;
         }
 
